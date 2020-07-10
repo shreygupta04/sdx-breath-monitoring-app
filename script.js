27e5bea2465c62d6425af954eff0c9b059924291
shreygupta04 <shreygupta04@gmail.com>
2020-07-09 22:18:54 -0400

    Added timer

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ ctx.lineWidth = 5;
 ctx.beginPath()
 
 var startX = 50
-var startY = height * 0.75 + 15
+var startY = height/2 + 115
 var speed = 10
 
 var down = 0;
@@ -21,19 +21,38 @@ var Keys = {
     right: false
 };
 
+var straight;
+var time;
+var forward;
+
 window.onkeydown = function(e) {
     var kc = e.keyCode;
     e.preventDefault();
     if (kc === 38) {
         Keys.up = true;    // so check exclusively
         Keys.right = false
+        clearInterval(straight)
+        $("p").text("00:00")
+        clearInterval(time)
+        forward = true
     }
     if (kc === 39) {
+        time = setInterval(updateTime, 1000)
         Keys.right = true;
         Keys.up = false
+        if(forward === true) {
+            straight = setInterval(straightLine, 20)
+            forward = false
+        }
     }
     
 };
+
+function straightLine() {
+    ctx.moveTo(startX, startY)
+    ctx.lineTo(startX += 1, startY)
+    ctx.stroke()
+}
 
 window.onkeyup = function(e) {
     var kc = e.keyCode;
@@ -44,10 +63,12 @@ window.onkeyup = function(e) {
     if(kc === 39) {
         down += 1
     }
-    console.log(down)
     Keys.up = Keys.right = true;
 };
-
+$(".chestbtn").addClass('btn-light').removeClass('btn-dark')
+        $(".abdomenbtn").addClass('btn-dark').removeClass('btn-light')
+        $(".abdomen").hide();
+        $(".chest").show();
 $(document).ready(function(){
     $(".chestbtn").click(function(){
         $(".chestbtn").addClass('btn-light').removeClass('btn-dark')
@@ -66,33 +87,33 @@ $(document).ready(function(){
 setInterval(check, 10)
 
 function check() {
+    if(startX  === $(window).width() - 50) {
+        startX = 50
+        ctx.beginPath()
+        ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    }
+
     if (Keys.up && Keys.right === false) {
-        if(startX  === $(window).width() - 50) {
-            startX = 50
-            ctx.beginPath()
-            ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-        }
         ctx.moveTo(startX, startY)
         if(down === 1) {
             
             ctx.lineTo(startX += 0.5, startY += 1)
         }
         else {
-            
             ctx.lineTo(startX += 0.5, startY -= 1)
         }
         ctx.stroke()
     }
-    else if (Keys.right && Keys.up === false) {
-        if(startX  === $(window).width() - 50) {
-            startX = 50
-            ctx.beginPath()
-            ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-        }
-        ctx.moveTo(startX, startY)
-        ctx.lineTo(startX += 1, startY)
-        ctx.stroke()
-    }
 }
 
-
+function updateTime() {
+    var newTime = parseInt($("p").text().slice(-2)) + 1
+    newTime = newTime.toString()
+    if(newTime.length === 2) {
+        console.log(newTime)
+        $("p").text("00:" + newTime.toString())
+    }
+    else {
+        $("p").text("00:0" + newTime.toString())
+    }
+}
